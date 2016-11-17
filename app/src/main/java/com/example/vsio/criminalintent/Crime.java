8acd1cace04e4c129ff5e8cb0bf4d61638695be1
Homaing <a470334311@gmail.com>
2016-11-17 20:00:21 +0800

    add a CrimeActivity

---
+++ app/src/main/java/com/example/vsio/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.example.vsio.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by vsio on 16-11-17.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
